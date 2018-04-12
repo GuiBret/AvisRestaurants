4d2d5ac796831958f3b0d4643e18c2fa099845bd
GuiBret <guillaume.bretzner@gmail.com>
2018-04-12 20:08:53 +0200

    fix(configuration): fixed paths to make the app work in localhost

--- js/configuration.js
+++ js/configuration.js
@@ -1,7 +1,8 @@
 class Configuration {
     constructor() {
         //this.lang = document.URL.match(/http(?:s?):\/\/.*\.com\/?(.*)\/mes-projets\/avisrest/)[1];
-        
+
+        this.mode = (new URL(window.location.href).hostname === "localhost") ? "LOCAL" : "SYMFONY"
         this.lang = this.parseURL();
         this.isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
 
@@ -18,9 +19,6 @@ class Configuration {
     parseURL() {
         let url = new URL(window.location.href);
 
-
-
-
-        return url.pathname.split("/")[1];
+        return (this.mode === "LOCAL") ? "fr" : url.pathname.split("/")[1];
     }
 }
