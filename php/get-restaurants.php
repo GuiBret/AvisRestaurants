66898fcf4eeaac7a0dd260de941fad1a86fb6446
GuiBret <guillaume.bretzner@gmail.com>
2018-05-12 16:03:52 +0200

    refactor(get-restaurants): small refactor
    remove useless code

--- php/get-restaurants.php
+++ php/get-restaurants.php
@@ -10,17 +10,13 @@ try {
 
     $apikey = getAPIKey();
 
+
     header("Content-Type: application/json");
-    
+
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     "Authorization:". "Bearer ". $apikey));
 
-
-
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Sert à supprimer le "1" en fin de chaîne pour parser le JSON correctement
-
-    $response = curl_exec($ch);
-
 
     $response = json_decode(curl_exec($ch));
 
